f7356ec19246c23e2576ead23a113cda7694817e
ZacZhang <zaczhang19@gmail.com>
2017-08-14 15:04:41 -0400

    3 restful api: getProblems & getProblem & postProblem finished

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var restRouter = require("./routes/rest");
+var mongoose = require("mongoose");
+
+// connect to MongoDb
+mongoose.connect("mongodb://user:user@ds139909.mlab.com:39909/coj");
 
 // if a http request starts from "/api/v1", then restRouter will handle it
 app.use("/api/v1", restRouter);
--- oj-server/services/problemService.js
+++ oj-server/services/problemService.js
@@ -41,6 +41,26 @@ var getProblems = function () {
     });
 }
 
+var getProblem = function (id) {
+  return new Promise((resolve, reject) => {
+    resolve(problems.find(problem => problem.id === id));
+  });
+}
+
+var addProblem = function (newProblem) {
+  return new Promise((resolve, reject) => {
+    if (problems.find(problem => problem.name === newProblem.name)) {
+      reject("Problem already exists");
+    } else {
+      newProblem.id = problems.length + 1;
+      problems.push(newProblem);
+      resolve(newProblem);
+    }
+  })
+}
+
 module.exports = {
-    getProblems: getProblems
+  getProblems: getProblems,
+  getProblem: getProblem,
+  addProblem: addProblem
 }
